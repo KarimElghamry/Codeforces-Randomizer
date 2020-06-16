a3f80e86ac72c2427058e07ba5d87c801d43e8d7
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 18:52:24 +0200

    add: styles to Tag

--- src/components/topics/Tag.tsx
+++ src/components/topics/Tag.tsx
@@ -1,15 +1,30 @@
 import React, {ReactElement} from 'react';
 import styled from 'styled-components';
 
-const StyledTag = styled.div`
-  background-color: #ebebeb;
-  border-radius: 5px;
-  color: #808080;
-  min-width: 50px;
+interface Props {
+  selected: boolean;
+}
+
+const StyledTag = styled.div<Props>`
+  background-color: ${(props) => (props.selected ? '#33AC71' : '#00bcd4')};
+  border-radius: 2px;
+  color: white;
+  min-width: 80px;
+  min-height: 30px;
+  margin: 5px;
+  user-select: none;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Tag: React.FC = (): ReactElement => {
-  return <StyledTag>KARIM</StyledTag>;
+const Tag: React.FC<Props> = (props: Props): ReactElement => {
+  return (
+    <StyledTag selected={props.selected}>
+      <div style={{margin: '5px'}}>binahjghjgry eotio</div>
+    </StyledTag>
+  );
 };
 
 export default Tag;
--- src/components/topics/Topics.tsx
+++ src/components/topics/Topics.tsx
@@ -1,18 +1,25 @@
 import React, {ReactElement} from 'react';
 import styled from 'styled-components';
+import Tag from './Tag';
 
 const Container = styled.div`
   width: 500px;
   height: 150px;
-  border: grey solid 2px;
+  border: grey solid 1px;
   border-radius: 10px;
   margin: 10px;
+  display: block;
 `;
 
 const Topics: React.FC = (): ReactElement => {
   return (
     <Container>
-      <div></div>
+      <Tag selected={false}></Tag>
+      <Tag selected={true}></Tag>
+      <Tag selected={true}></Tag>
+      <Tag selected={true}></Tag>
+      <Tag selected={false}></Tag>
+      <Tag selected={false}></Tag>
     </Container>
   );
 };
