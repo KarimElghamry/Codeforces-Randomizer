fd4ecd56a472e92a04eb16effad6f2d320812f34
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 14:33:01 +0200

    add: SnackbarProps interface

--- src/components/snackbar/Snackbar.tsx
+++ src/components/snackbar/Snackbar.tsx
@@ -1,7 +1,30 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
 
-const Snackbar: React.FC = (): ReactElement => {
-  return <div></div>;
+interface SnackbarProps {
+  visible: boolean;
+  type?: string;
+  content: string;
+}
+
+const Wrapper: React.FC<SnackbarProps> = (
+  props: SnackbarProps
+): ReactElement => {
+  return <div>{props.content}</div>;
 };
+
+Wrapper.defaultProps = {
+  type: 'error',
+};
+
+const Snackbar = styled(Wrapper)`
+  background-color: ${(props) => (props.type === 'error' ? 'red' : 'green')};
+  opacity: 0.7;
+  position: absolute;
+  bottom: 20px;
+  min-width: 100px;
+  min-height: 55px;
+  border-radius: 20px;
+`;
 
 export default Snackbar;
