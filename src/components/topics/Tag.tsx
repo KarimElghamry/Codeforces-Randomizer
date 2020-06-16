a3e498db5a49781ba5ff74c4c84ce6c6e4967ec4
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 18:27:45 +0200

    add: container styling to tag

--- src/components/topics/Tag.tsx
+++ src/components/topics/Tag.tsx
@@ -1,11 +1,15 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
+
+const StyledTag = styled.div`
+  background-color: #ebebeb;
+  border-radius: 5px;
+  color: #808080;
+  min-width: 50px;
+`;
 
 const Tag: React.FC = (): ReactElement => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+  return <StyledTag>KARIM</StyledTag>;
 };
 
 export default Tag;
