774cf3f105efc75c3567ae3ceacb45866fff6365
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 18:10:47 +0200

    add: test to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,18 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
 import * as assets from '../../assets';
+
+const CenteredDiv = styled.div`
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+`;
 
 const Header: React.FC = (): ReactElement => {
   return (
     <div>
       <img width={300} src={assets.images.codeforcesLogo} alt=""></img>
+      <CenteredDiv>Randomizer</CenteredDiv>
     </div>
   );
 };
