96bcafa68d110c80f2eee1a17e8f5a26b4797a08
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 18:07:39 +0200

    add: image to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {ReactElement, useState} from 'react';
 import Snackbar from './components/snackbar/Snackbar';
+import Header from './components/header/Header';
 
 const App: React.FC<{}> = (): ReactElement => {
   const content: string = 'emiwnewienmwe';
   const [visible, setVisible] = useState<boolean>(true);
   return (
     <div>
+      <Header></Header>
       <button onClick={() => setVisible(true)}>CLICK</button>
       <Snackbar
         type="error"
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import * as assets from '../../assets';
 const Header: React.FC = (): ReactElement => {
   return (
     <div>
-      <img src={assets.images.codeforcesLogo} alt=""></img>
+      <img width={300} src={assets.images.codeforcesLogo} alt=""></img>
     </div>
   );
 };
