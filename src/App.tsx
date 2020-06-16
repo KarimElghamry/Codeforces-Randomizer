c177ac7e1f541a5b17fdbf9a96a286be1cfa02f7
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 18:19:50 +0200

    add: container to topics section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, {ReactElement, useState} from 'react';
 import Snackbar from './components/snackbar/Snackbar';
 import Header from './components/header/Header';
+import Topics from './components/topics/Topics';
 
 const App: React.FC<{}> = (): ReactElement => {
   const content: string = 'emiwnewienmwe';
   const [visible, setVisible] = useState<boolean>(true);
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <Header></Header>
       <button onClick={() => setVisible(true)}>CLICK</button>
+      <Topics>s;dls;dl</Topics>
       <Snackbar
         type="error"
         content={content}
--- src/components/topics/Topics.tsx
+++ src/components/topics/Topics.tsx
@@ -1,10 +1,19 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 500px;
+  height: 150px;
+  border: grey solid 2px;
+  border-radius: 10px;
+  margin: 10px;
+`;
 
 const Topics: React.FC = (): ReactElement => {
   return (
-    <div>
+    <Container>
       <div></div>
-    </div>
+    </Container>
   );
 };
 
