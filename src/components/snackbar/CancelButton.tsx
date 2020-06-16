85c369d8549af42b842a32e7d860288d0756a8c0
KarimElghamry <Karimelghamry@gmail.com>
2020-06-16 15:01:12 +0200

    add: styling to CancelButton

--- src/components/snackbar/CancelButton.tsx
+++ src/components/snackbar/CancelButton.tsx
@@ -1,11 +1,24 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
 
 interface Props {
   onClick: Function;
 }
 
+const StyleCancelButton = styled.div`
+  background-color: white;
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const CancelButton: React.FC<Props> = (props: Props): ReactElement => {
-  return <div></div>;
+  return (
+    <StyleCancelButton onClick={() => props.onClick()}></StyleCancelButton>
+  );
 };
 
 export default CancelButton;
