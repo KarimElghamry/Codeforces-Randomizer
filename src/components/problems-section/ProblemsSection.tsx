075579a8b61dfb82032e0c0aca295792adfb1e51
KarimElghamry <Karimelghamry@gmail.com>
2020-06-17 15:29:01 +0200

    add: ProblemsSection file

---
+++ src/components/problems-section/ProblemsSection.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const ProblemsSection: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ProblemsSection;
