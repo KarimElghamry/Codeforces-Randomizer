17f8e15e69664071add7a38c31fdd884f9b05141
KarimElghamry <Karimelghamry@gmail.com>
2020-06-17 14:23:59 +0200

    add: LoadingIndicator file

---
+++ src/components/randomize-button/LoadingIndicator.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const LoadingIndicator: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingIndicator;
