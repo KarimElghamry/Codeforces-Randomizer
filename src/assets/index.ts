7eb1005228e15ebbae5ac22f484d33c2d4db1ae9
KarimElghamry <Karimelghamry@gmail.com>
2020-06-17 14:01:46 +0200

    add: loop icon to asssets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 import codeforcesLogo from './codeforces_logo.png';
+import loopIcon from './loop.svg';
 
-export const images: {codeforcesLogo: string} = {
+export const images: {codeforcesLogo: string; loopIcon: string} = {
   codeforcesLogo: codeforcesLogo,
+  loopIcon: loopIcon,
 };
