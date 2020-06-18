5d99e7842de0334fcd54323d3d1e091e491c1428
KarimElghamry <Karimelghamry@gmail.com>
2020-06-18 13:25:57 +0200

    add: setProblemsList service

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,7 +1,26 @@
-export function getPromblemsList() {
+import {Problem} from '../models/Problem';
+import {ProblemStatistics} from '../models/ProblemStatistics';
+
+export function getPromblemsList(): Array<{
+  problem: Problem;
+  problemStatistics: ProblemStatistics;
+}> {
   const result: string | null = localStorage.getItem('problemsList');
   if (!result) return [];
 
-  const problemsList: Array<any> = JSON.parse(result).problemsList;
+  const problemsList: Array<{
+    problem: Problem;
+    problemStatistics: ProblemStatistics;
+  }> = JSON.parse(result).problemsList;
   return problemsList;
+}
+
+export function setProblemsList(
+  list: Array<{problem: Problem; problemStatistics: ProblemStatistics}>
+) {
+  const storageObject = {
+    problemsList: list,
+  };
+
+  localStorage.setItem('problemsList', JSON.stringify(storageObject));
 }
