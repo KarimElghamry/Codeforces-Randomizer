3a910c63263a049a4a0bd6aa3d80ffe9e1c64bcc
KarimElghamry <Karimelghamry@gmail.com>
2020-06-18 13:53:30 +0200

    add: style to clear button

--- src/components/clear-button/ClearButton.tsx
+++ src/components/clear-button/ClearButton.tsx
@@ -1,11 +1,23 @@
 import React, {ReactElement} from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.div`
+  width: 120px;
+  height: 35px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: red;
+  color: white;
+  font-weight: 600;
+  border-radius: 6px;
+  user-select: none;
+  cursor: pointer;
+  transition-duration: 0.3s;
+`;
 
 const ClearButton: React.FC = (): ReactElement => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+  return <StyledButton>Clear</StyledButton>;
 };
 
 export default ClearButton;
