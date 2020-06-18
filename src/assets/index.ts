271b3725356474c30f84320977192306fb308f8a
KarimElghamry <Karimelghamry@gmail.com>
2020-06-18 20:16:40 +0200

    add: github logo to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
 import codeforcesLogo from './codeforces_logo.png';
 import userIcon from './user.png';
+import githubLogo from './gh-logo.png';
 import {ReactComponent as loopIcon} from './loop.svg';
 
 export const images = {
   codeforcesLogo: codeforcesLogo,
   loopIcon: loopIcon,
   userIcon: userIcon,
+  githubLogo: githubLogo,
 };
