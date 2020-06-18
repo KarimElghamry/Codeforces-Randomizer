7334691cb086a1e34cdabe3dc8dc017bce90c5df
KarimElghamry <Karimelghamry@gmail.com>
2020-06-18 17:19:04 +0200

    add: Slider styling

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,12 @@
 import React, {ReactElement, useState} from 'react';
-import {Range} from 'react-range';
+import {Range, getTrackBackground} from 'react-range';
 import styled from 'styled-components';
+
+interface TrackProps {
+  values: Array<number>;
+  min: number;
+  max: number;
+}
 
 const Container = styled.div`
   display: flex;
@@ -11,20 +17,38 @@ const Container = styled.div`
   margin: 20px;
 `;
 
-const Track = styled.div`
-  background-color: #d5d5d5;
+const Track = styled.div<TrackProps>`
+  background: ${(props) =>
+    getTrackBackground({
+      values: props.values,
+      colors: ['#ccc', '#198FCE', '#ccc'],
+      min: props.min,
+      max: props.max,
+    })};
   width: 100%;
   height: 2px;
   margin-bottom: 10px;
 `;
 
-const Thumb = styled.div`
-  background-color: #c8cac5;
-  height: 20px;
-  width: 20px;
-  border-radius: 20px;
-  box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.75);
+const Thumb = styled.div<{index: number}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => (props.index === 0 ? '#F8CC52' : '#BA1D25')};
+  height: 16px;
+  width: 16px;
+  border-radius: 16px;
 `;
+
+const Indicator = styled.div<{isDragged: boolean}>`
+  height: 8px;
+  width: 8px;
+  border-radius: 6px;
+  background-color: ${(props) => (props.isDragged ? '#198FCE' : 'white')};
+`;
+
+const min = 800;
+const max = 3500;
 
 const Slider: React.FC<{}> = (): ReactElement => {
   const [values, setValues] = useState<Array<number>>([800, 3500]);
@@ -32,14 +56,22 @@ const Slider: React.FC<{}> = (): ReactElement => {
     <Container>
       <Range
         step={100}
-        min={800}
-        max={3500}
+        min={min}
+        max={max}
         values={values}
         onChange={(values) => setValues(values)}
         renderTrack={({props, children}) => (
-          <Track {...props}>{children}</Track>
+          <Track min={min} max={max} values={values} {...props}>
+            {children}
+          </Track>
         )}
-        renderThumb={({props}) => <Thumb {...props} />}
+        renderThumb={({props, isDragged, index}) => {
+          return (
+            <Thumb {...props} index={index}>
+              <Indicator isDragged={isDragged}></Indicator>
+            </Thumb>
+          );
+        }}
       />
       <div>{`Rating: ${values[0]} - ${values[1]}`}</div>
     </Container>
